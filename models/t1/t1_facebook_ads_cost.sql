fcbfd3f176629770b2cf8c23b2d54f2c49be9a6c
Huy ta <data@maxeagle.net>
2025-05-06 07:03:01 +0000

    aaa

---
+++ models/t1/t1_facebook_ads_cost.sql
@@ -0,0 +1,5 @@
+SELECT * FROM ume_facebook_ads_dwh.facebook_ads_ads_insights
+UNION ALL
+SELECT * FROM chaching_facebook_ads_dwh.facebook_ads_ads_insights
+UNION ALL
+SELECT * FROM lyb_facebook_ads_dwh.facebook_ads_ads_insights
---
+++ models/t1/t1_tiktok_ads_cost.sql
@@ -0,0 +1,5 @@
+SELECT * FROM tiktok_ads_dwh.chaching_tiktok_ads_dwh
+UNION ALL
+SELECT * FROM tiktok_ads_dwh.lyb_tiktok_ads_dwh
+UNION ALL
+SELECT * FROM tiktok_ads_dwh.ume_tiktok_ads_dwh
