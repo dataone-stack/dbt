84c9ef47324bce957b2d41dd44c4b81d9d0d9a01
Huy ta <data@maxeagle.net>
2025-05-07 16:28:23 +0000

    ádf

--- models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
+++ models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
@@ -1,22 +1,21 @@
-with ladi_agg as (
-  select 
-    date_insert,
-    id_staff,
-    manager,
-    brand,
-    channel,
-    sum(doanhThuLadi) as doanhThuLadi
-  from {{ref("t2_ladipage_facebook_total")}}
-  group by date_insert, id_staff, manager, brand, channel
+WITH ladi_aggregated AS (
+    SELECT 
+        date_insert,
+        id_staff,
+        manager,
+        brand,
+        channel,
+        SUM(doanhThuLadi) AS doanhThuLadi
+    FROM {{ ref("t2_ladipage_facebook_total") }}
+    GROUP BY date_insert, id_staff, manager, brand, channel
 )
-
-select 
-  ads.*,
-  ladi_agg.doanhThuLadi
-from {{ref("t3_ads_total_with_tkqc")}} as ads
-left join ladi_agg
-on ads.date_start = ladi_agg.date_insert 
-and ads.ma_nhan_vien = ladi_agg.id_staff 
-and ads.manager = ladi_agg.manager 
-and ads.brand = ladi_agg.brand 
-and ads.channel = ladi_agg.channel
+SELECT 
+    ads.*,
+    COALESCE(ladi_aggregated.doanhThuLadi, 0) AS doanhThuLadi
+FROM {{ ref("t3_ads_total_with_tkqc") }} AS ads
+LEFT JOIN ladi_aggregated
+    ON ads.date_start = ladi_aggregated.date_insert 
+    AND ads.ma_nhan_vien = ladi_aggregated.id_staff 
+    AND ads.manager = ladi_aggregated.manager 
+    AND ads.brand = ladi_aggregated.brand 
+    AND ads.channel = ladi_aggregated.channel
