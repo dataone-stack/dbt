2cdaed695275117367e128392e414f0d241262b7
Huy ta <data@maxeagle.net>
2025-05-07 12:57:34 +0000

    dsf

--- models/t1/t1_pancake_pos_order_total.sql
+++ models/t1/t1_pancake_pos_order_total.sql
@@ -4,4 +4,4 @@ select * , 'LYB' as brand from `lyb_pancake_pos_dwh.pancake_order`
 union all
 select * , 'UME' as brand from `ume_pancake_pos_dwh.pancake_order`
 union all
-select * , 'LYB Cosmetic' as brand from `lybcosmetics_pancake_pos_dwh.pancake_order`
+select * , 'LYB Cosmetics' as brand from `lybcosmetics_pancake_pos_dwh.pancake_order`
