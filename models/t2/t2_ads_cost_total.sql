a6fefbf3fa919e45357c574920d9ae77bf393dad
Huy ta <data@maxeagle.net>
2025-05-07 10:50:03 +0000

    sàdsaf

--- models/t2/t2_ads_cost_total.sql
+++ models/t2/t2_ads_cost_total.sql
@@ -1,5 +1,18 @@
-SELECT date(date_start) as date_start,account_id,spend FROM  {{ ref('t1_facebook_ads_total')}}
+SELECT date(date_start) as date_start,account_id,spend,COALESCE(
+    CAST(
+      JSON_VALUE(
+        (
+          SELECT value
+          FROM UNNEST(fb.action_values) AS value
+          WHERE JSON_VALUE(value, '$.action_type') = 'onsite_conversion.purchase'
+          LIMIT 1
+        ),
+        '$.value'
+      ) AS INT64
+    ),
+    0
+  )) AS doanhThuAds, FROM  {{ ref('t1_facebook_ads_total')}}
 union all
-select date(stat_time_day) as date_start ,account_id,spend From {{ref("t1_tiktok_ads_total")}}
+select date(stat_time_day) as date_start ,account_id,spend,int64(total_onsite_shopping_value) as doanhThuAds as  From {{ref("t1_tiktok_ads_total")}}
 union all
-select date(stat_time_day) as date_start ,account_id,spend From {{ref("t1_tiktokGMV_ads_total")}}
+select date(stat_time_day) as date_start ,account_id,spend, 0 as doanhThuAds From {{ref("t1_tiktokGMV_ads_total")}}
