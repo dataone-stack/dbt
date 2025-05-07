165af13f0120116637dd184aaa334b6adaf9b6f0
Huy ta <data@maxeagle.net>
2025-05-07 08:29:04 +0000

    ấdf

---
+++ models/t1/t1_pancake_pos_order_total.sql
@@ -0,0 +1,7 @@
+select *,'Chaching' as brand from `chaching_pancake_pos_dwh.pancake_order`
+union all
+select * , 'LYB' as brand from `lyb_pancake_pos_dwh.pancake_order`
+union all
+select * , 'UME' as brand from `ume_pancake_pos_dwh.pancake_order`
+union all
+select * , 'LYB Cosmetic' as brand from `lybcosmetics_pancake_pos_dwh.pancake_order`
---
+++ models/t1/t1_shopee_ads_total.sql
@@ -0,0 +1,7 @@
+select *,'Chaching' as brand from `chaching_shopee_ads_dwh.shopee_cpc_ads_daily_performance_chaching_brand`
+union all
+select *,'LYB' as brand from `lyb_shopee_ads_dwh.shopee_cpc_ads_daily_performance_lyb_official`
+union all
+select *,'UME' as brand from `ume_shopee_ads_dwh.shopee_cpc_ads_daily_performance_ume_viet_nam`
+union all
+select *,'An Cung' as brand from `ancung_shopee_ads_dwh.shopee_cpc_ads_daily_performance_ancung_brand`
