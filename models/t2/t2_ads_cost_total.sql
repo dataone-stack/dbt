3ea5d0b6629d8a6044df97010babb87f07398422
Huy ta <data@maxeagle.net>
2025-05-07 10:55:41 +0000

    dsa

--- models/t2/t2_ads_cost_total.sql
+++ models/t2/t2_ads_cost_total.sql
@@ -13,6 +13,6 @@ SELECT date(date_start) as date_start,account_id,spend,COALESCE(
     0
   ) AS doanhThuAds FROM  {{ ref('t1_facebook_ads_total')}}
 union all
-select date(stat_time_day) as date_start ,account_id,spend,int64(total_onsite_shopping_value) as doanhThuAds From {{ref("t1_tiktok_ads_total")}}
+select date(stat_time_day) as date_start ,account_id,spend,total_onsite_shopping_value as doanhThuAds From {{ref("t1_tiktok_ads_total")}}
 union all
 select date(stat_time_day) as date_start ,account_id,spend, 0 as doanhThuAds From {{ref("t1_tiktokGMV_ads_total")}}
