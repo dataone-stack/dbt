ba4e9fa255b4c7da61187466d89ba251abfe53e1
Huy ta <data@maxeagle.net>
2025-05-07 16:10:06 +0000

    sfdg

--- models/t3/t3_ads_total_with_tkqc.sql
+++ models/t3/t3_ads_total_with_tkqc.sql
@@ -7,7 +7,7 @@ tkqc.staff,
 tkqc.manager, 
 tkqc.brand,
 tkqc.channel, 
-ads.spendas chiPhiAds, 
+ads.spend chiPhiAds, 
 doanhThuAds as doanhThuAds 
 from {{ ref('t2_ads_total') }} as ads right JOIN {{ ref('t1_tkqc')}} as tkqc
 ON cast(ads.account_id as string) = tkqc.idtkqc
