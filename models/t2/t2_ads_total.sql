da343b5a540ae1c394203f179cf8c6231be2b259
Huy ta <data@maxeagle.net>
2025-05-07 02:51:08 +0000

    da

--- models/t2/t2_ads_total.sql
+++ models/t2/t2_ads_total.sql
@@ -1,4 +1,4 @@
-SELECT date_start as stat_time_date,account_id,spend FROM  {{ ref('t1_facebook_ads_cost')}}
+SELECT date_start ,account_id,spend FROM  {{ ref('t1_facebook_ads_cost')}}
 union all
 select stat_time_date,account_id,spend From {{ref("t1_tiktok_ads_cost")}}
 union all
