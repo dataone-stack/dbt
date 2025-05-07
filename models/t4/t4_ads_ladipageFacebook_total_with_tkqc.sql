53774d269743d84ce4c46a09030c4aee98083908
Huy ta <data@maxeagle.net>
2025-05-07 15:42:50 +0000

    sadf

--- models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
+++ models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
@@ -1,7 +1,6 @@
 select ads.*,
-sum(ladi.doanhThuLadi) as doanhThuLadi
+ladi.doanhThuLadi
 from {{ref("t3_ads_total_with_tkqc")}} as ads
-left join {{ref("t2_ladipage_facebook_total")}} as ladi
+inner join {{ref("t2_ladipage_facebook_total")}} as ladi
 on ads.date_start = ladi.date_insert and ads.ma_nhan_vien = ladi.id_staff 
 and ads.manager = ladi.manager and ads.brand = ladi.brand and ads.channel = ladi.channel
-group by ads.date_start,ads.idtkqc,ads.nametkqc,ads.ma_nhan_vien,ads.staff,ads.manager, ads.brand,ads.channel,ads.chiPhiAds,ads.doanhThuAds
