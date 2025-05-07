113f359bc2551c677ac7943bfeb8548343dd2b09
Huy ta <data@maxeagle.net>
2025-05-07 16:46:46 +0000

    ád

--- models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
+++ models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
@@ -1,16 +1,6 @@
-SELECT 
-    ads.*,
-    CASE 
-        WHEN ROW_NUMBER() OVER (
-            PARTITION BY ads.date_start, ads.ma_nhan_vien, ads.manager, ads.brand, ads.channel 
-            ORDER BY ladi.date_insert
-        ) = 1 THEN ladi.doanhThuLadi 
-        ELSE 0 
-    END AS doanhThuLadi
-FROM {{ref("t3_ads_total_with_tkqc")}} AS ads
-LEFT JOIN {{ref("t2_ladipage_facebook_total")}} AS ladi
-    ON ads.date_start = ladi.date_insert 
-    AND ads.ma_nhan_vien = ladi.id_staff 
-    AND ads.manager = ladi.manager 
-    AND ads.brand = ladi.brand 
-    AND ads.channel = ladi.channel
+select ads.*,
+ladi.doanhThuLadi as doanhThuLadi
+from {{ref("t3_ads_total_with_tkqc")}} as ads
+left join {{ref("t2_ladipage_facebook_total")}} as ladi
+on ads.date_start = ladi.date_insert and ads.ma_nhan_vien = ladi.id_staff 
+and ads.manager = ladi.manager and ads.brand = ladi.brand and ads.channel = ladi.channel
