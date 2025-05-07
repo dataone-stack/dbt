d819cd53c29892545300dcaa63ca36f906801adf
Huy ta <data@maxeagle.net>
2025-05-07 17:24:44 +0000

    sdf

--- models/t5/t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc.sql
+++ models/t5/t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc.sql
@@ -8,4 +8,4 @@ SELECT
 FROM {{ref("t4_ads_ladipageFacebook_total_with_tkqc")}} AS ads 
 LEFT JOIN {{ref("t1_tiktokGMV_ads_doanhThu_total")}} AS gmv 
     ON gmv.date_start = ads.date_start 
-    AND gmv.account_id = ads.idtkqc
+    AND cast(gmv.account_id as string) = ads.idtkqc
