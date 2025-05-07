949af863bdf77c557aef04d66bbd038acb81f9cb
Huy ta <data@maxeagle.net>
2025-05-07 17:39:54 +0000

    asd

--- models/t5/t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc.sql
+++ models/t5/t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc.sql
@@ -1,19 +1,16 @@
 SELECT 
-    ads.date_start,
-    ads.idtkqc,
-    ads.nametkqc,
-    ads.ma_nhan_vien,
-    ads.staff,
-    ads.manager,
-    ads.brand,
-    ads.channel,
-    ads.chiPhiAds,
-    ads.doanhThuLadi,
-    CASE 
-        WHEN gmv.doanhThuAds IS NOT NULL 
-        THEN ads.doanhThuAds + COALESCE(cast(gmv.doanhThuAds as int64), 0)
-        ELSE ads.doanhThuAds
-    END AS doanhThuAds
+    -- ads.date_start,
+    -- ads.idtkqc,
+    -- ads.nametkqc,
+    -- ads.ma_nhan_vien,
+    -- ads.staff,
+    -- ads.manager,
+    -- ads.brand,
+    -- ads.channel,
+    -- ads.chiPhiAds,
+    -- ads.doanhThuLadi,
+    ads.*,
+    gmv.doanhThuAds as doanhThuGMVTiktok
 FROM {{ref("t4_ads_ladipageFacebook_total_with_tkqc")}} AS ads 
 LEFT JOIN {{ref("t1_tiktokGMV_ads_doanhThu_total")}} AS gmv 
     ON gmv.date_start = ads.date_start 
