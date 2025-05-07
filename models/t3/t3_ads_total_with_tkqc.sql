afeddeccd99c3cd3cc617a31128dcc75af27b072
Huy ta <data@maxeagle.net>
2025-05-07 04:38:19 +0000

    dasd

--- models/t3/t3_ads_total_with_tkqc.sql
+++ models/t3/t3_ads_total_with_tkqc.sql
@@ -1,3 +1,4 @@
 select ads.date_start,tkqc.idtkqc,tkqc.nametkqc,tkqc.ma_nhan_vien,tkqc.staff,tkqc.manager, tkqc.brand,tkqc.channel, sum(ads.spend) as chiPhiAds from {{ ref('t2_ads_total') }} as ads
 LEFT JOIN {{ ref('t1_tkqc')}} as tkqc
 ON cast(ads.account_id as string) = tkqc.idtkqc
+group by ads.date_start,tkqc.idtkqc,tkqc.nametkqc,tkqc.ma_nhan_vien,tkqc.staff,tkqc.manager, tkqc.brand,tkqc.channel
