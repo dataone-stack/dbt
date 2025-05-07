17d29ac3ac91fdd09322c29d797b67f32fe0d854
Huy ta <data@maxeagle.net>
2025-05-07 15:46:57 +0000

    dá

--- models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
+++ models/t4/t4_ads_ladipageFacebook_total_with_tkqc.sql
@@ -1,6 +1,6 @@
 select ads.*,
 ladi.doanhThuLadi
 from {{ref("t3_ads_total_with_tkqc")}} as ads
-inner join {{ref("t2_ladipage_facebook_total")}} as ladi
+left join {{ref("t2_ladipage_facebook_total")}} as ladi
 on ads.date_start = ladi.date_insert and ads.ma_nhan_vien = ladi.id_staff 
 and ads.manager = ladi.manager and ads.brand = ladi.brand and ads.channel = ladi.channel
