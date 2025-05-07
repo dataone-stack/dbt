71915c5f70739f78304b3fb98cd97c0a3a2f6a59
Huy ta <data@maxeagle.net>
2025-05-07 16:09:13 +0000

    dá

--- models/t3/t3_ads_total_with_tkqc.sql
+++ models/t3/t3_ads_total_with_tkqc.sql
@@ -7,8 +7,7 @@ tkqc.staff,
 tkqc.manager, 
 tkqc.brand,
 tkqc.channel, 
-sum(ads.spend)as chiPhiAds, 
-sum(doanhThuAds) as doanhThuAds 
+ads.spendas chiPhiAds, 
+doanhThuAds as doanhThuAds 
 from {{ ref('t2_ads_total') }} as ads right JOIN {{ ref('t1_tkqc')}} as tkqc
 ON cast(ads.account_id as string) = tkqc.idtkqc
-group by ads.date_start,tkqc.idtkqc,tkqc.nametkqc,tkqc.ma_nhan_vien,tkqc.staff,tkqc.manager, tkqc.brand,tkqc.channel
