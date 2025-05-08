975326fbf4000d1aacb2e32b50c016b113898806
Huy ta <data@maxeagle.net>
2025-05-08 06:05:01 +0000

    ads

--- models/t1/t1_shopee_ads_total.sql
+++ models/t1/t1_shopee_ads_total.sql
@@ -1,7 +1,7 @@
-select *,'Chaching' as brand,'1' as idtkqc from `chaching_shopee_ads_dwh.shopee_cpc_ads_daily_performance_chaching_brand`
+select *,'1' as idtkqc from `chaching_shopee_ads_dwh.shopee_cpc_ads_daily_performance_chaching_brand`
 union all
-select *,'LYB' as brand,'2' as idtkqc from `lyb_shopee_ads_dwh.shopee_cpc_ads_daily_performance_lyb_official`
+select *,'2' as idtkqc from `lyb_shopee_ads_dwh.shopee_cpc_ads_daily_performance_lyb_official`
 union all
-select *,'UME' as brand,'3' as idtkqc from `ume_shopee_ads_dwh.shopee_cpc_ads_daily_performance_ume_viet_nam`
+select *,'3' as idtkqc from `ume_shopee_ads_dwh.shopee_cpc_ads_daily_performance_ume_viet_nam`
 union all
-select *,'An Cung' as brand,'4' as idtkqc from `ancung_shopee_ads_dwh.shopee_cpc_ads_daily_performance_ancung_brand`
+select *,'4' as idtkqc from `ancung_shopee_ads_dwh.shopee_cpc_ads_daily_performance_ancung_brand`
--- models/t2/t2_ads_total.sql
+++ models/t2/t2_ads_total.sql
@@ -16,3 +16,5 @@ union all
 select date(stat_time_day) as date_start ,account_id,spend,cast(total_onsite_shopping_value as FLOAT64) as doanhThuAds From {{ref("t1_tiktok_ads_total")}}
 union all
 select date(stat_time_day) as date_start ,account_id,spend, 0 as doanhThuAds From {{ref("t1_tiktokGMV_ads_total")}}
+union all
+select date(date) as date_start ,idtkqc as account_id,expense as spend, broad_gmv as doanhThuAds From {{ref("t1_shopee_ads_total")}}
