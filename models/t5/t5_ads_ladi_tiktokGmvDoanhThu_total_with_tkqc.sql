5bfa77fbea3b7b81444b74883190c93f05034d4d
Huy ta <data@maxeagle.net>
2025-05-08 08:42:23 +0000

    ssdaf

--- models/t5/t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    ads.*,
-    gmv.doanhThuAds as doanhThuGMVTiktok
-FROM {{ref("t4_ads_ladipageFacebook_total_with_tkqc")}} AS ads 
-LEFT JOIN {{ref("t1_tiktokGMV_ads_doanhThu_total")}} AS gmv 
-    ON gmv.date_start = ads.date_start 
-    AND cast(gmv.account_id as string) = ads.idtkqc
--- models/t6/t6_ads_ladi_gmv_shopeeSearch_total_with_tkqc.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    ads.*,
-    shopeeSearch.doanhThuAds as doanhThuShopeeSearch
-FROM {{ref("t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc")}} AS ads 
-LEFT JOIN {{ref("t1_shopee_search_ads_total")}} AS shopeeSearch 
-    ON shopeeSearch.date_start = ads.date_start 
-    AND cast(shopeeSearch.account_id as string) = ads.idtkqc
