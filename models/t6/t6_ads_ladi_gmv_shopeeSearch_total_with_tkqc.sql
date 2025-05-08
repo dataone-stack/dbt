0c84c70caaa9ecc72494e863e736dab32d883919
Huy ta <data@maxeagle.net>
2025-05-08 07:51:33 +0000

    asd

--- models/t6/t6_ads_ladi_gmv_shopeeSearch_total_with_tkqc.sql
+++ models/t6/t6_ads_ladi_gmv_shopeeSearch_total_with_tkqc.sql
@@ -1,6 +1,6 @@
 SELECT 
     ads.*,
-    gmv.doanhThuAds as doanhThuShopeeSearch
+    shopeeSearch.doanhThuAds as doanhThuShopeeSearch
 FROM {{ref("t5_ads_ladi_tiktokGmvDoanhThu_total_with_tkqc")}} AS ads 
 LEFT JOIN {{ref("t1_shopee_search_ads_total")}} AS shopeeSearch 
     ON shopeeSearch.date_start = ads.date_start 
