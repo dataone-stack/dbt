348eba569b280d149397b8411c2f39a4a608ceb9
Huy ta <data@maxeagle.net>
2025-05-09 08:51:45 +0000

    asdf

--- models/t3/t3_doi_soat_ads_pos.sql
+++ models/t3/t3_doi_soat_ads_pos.sql
@@ -3,9 +3,9 @@ SELECT
     a.brand,
     a.channel,
     COALESCE(a.doanhThuShopeeSearch, 0) + COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0) AS totalAds,
-    p.total_price_after_sub_discount AS totalDoanhThuPos
+    sum(p.total_price_after_sub_discount) AS totalDoanhThuPos
 FROM {{ ref('t3_ads_total_with_tkqc') }} a
-FULL OUTER JOIN {{ ref('t1_pancake_pos_order_total') }} p
+left JOIN {{ ref('t1_pancake_pos_order_total') }} p
     ON a.date_start = DATE(p.inserted_at)
     AND a.brand = p.brand
     AND a.channel = CASE 
