86808fa2ed66e2be00fe4ad2d1996d95bdda6b6e
Huy ta <data@maxeagle.net>
2025-05-09 08:13:32 +0000

    sad

--- models/t3/t3_doi_soat_ads_pos.sql
+++ models/t3/t3_doi_soat_ads_pos.sql
@@ -2,14 +2,19 @@ SELECT
     a.date_start,
     a.brand,
     a.channel,
-    (a.doanhThuAds + a.doanhThuLadi + a.doanhThuGMVTiktok) AS totalAds,
-    sum(p.total_price_after_sub_discount)
-FROM {{ref("t3_ads_total_with_tkqc")}} a
-full outer join {{(ref("t1_pancake_pos_order_total"))}} p
-    ON a.date_start = date(p.inserted_at)
+    (COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0)) AS totalAds,
+    SUM(p.total_price_after_sub_discount) AS totalDoanhThuPos
+FROM {{ ref('t3_ads_total_with_tkqc') }} a
+FULL OUTER JOIN {{ ref('t1_pancake_pos_order_total') }} p
+    ON a.date_start = DATE(p.inserted_at)
     AND a.brand = p.brand
-    AND a.channel = p.order_sources_name
-group by a.date_start,
+    AND a.channel = CASE 
+                        WHEN p.order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
+                        WHEN p.order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
+                        ELSE p.order_sources_name 
+                    END
+GROUP BY 
+    a.date_start,
     a.brand,
     a.channel,
     totalAds
