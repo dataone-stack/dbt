a474bb3c0305bece9f9540f33a775ca2c21030c4
Huy ta <data@maxeagle.net>
2025-05-09 08:39:24 +0000

    sad

--- models/t3/t3_doi_soat_ads_pos.sql
+++ models/t3/t3_doi_soat_ads_pos.sql
@@ -2,8 +2,8 @@ SELECT
     a.date_start,
     a.brand,
     a.channel,
-    sum((COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0))) AS totalAds,
-    sum(p.total_price_after_sub_discount) AS totalDoanhThuPos
+    COALESCE(a.doanhThuShopeeSearch, 0) + (COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0)) AS totalAds,
+    p.total_price_after_sub_discount AS totalDoanhThuPos
 FROM {{ ref('t3_ads_total_with_tkqc') }} a
 FULL OUTER JOIN {{ ref('t1_pancake_pos_order_total') }} p
     ON a.date_start = DATE(p.inserted_at)
@@ -13,6 +13,3 @@ FULL OUTER JOIN {{ ref('t1_pancake_pos_order_total') }} p
                         WHEN p.order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
                         ELSE p.order_sources_name 
                     END
-group by  a.date_start,
-    a.brand,
-    a.channel
