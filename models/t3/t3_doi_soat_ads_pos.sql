2e92bc7485d17df609a49283bbe440a05321ed50
Huy ta <data@maxeagle.net>
2025-05-09 08:10:11 +0000

    sadf

--- models/t3/t3_doi_soat_ads_pos.sql
+++ models/t3/t3_doi_soat_ads_pos.sql
@@ -6,9 +6,16 @@ SELECT
     a.doanhThuLadi,
     a.doanhThuGMVTiktok,
     (a.doanhThuAds + a.doanhThuLadi + a.doanhThuGMVTiktok) AS totalAds,
-    p.total_price_after_sub_discount
+    sum(p.total_price_after_sub_discount)
 FROM {{ref("t3_ads_total_with_tkqc")}} a
 full outer join {{(ref("t1_pancake_pos_order_total"))}} p
     ON a.date_start = date(p.inserted_at)
     AND a.brand = p.brand
     AND a.channel = p.order_sources_name
+group by a.date_start,
+    a.brand,
+    a.channel,
+    a.doanhThuAds,
+    a.doanhThuLadi,
+    a.doanhThuGMVTiktok,
+    totalAds
