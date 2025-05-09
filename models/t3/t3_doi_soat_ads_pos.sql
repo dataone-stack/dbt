855fff657fabb5f16ec2ac61ff1b7726b9c0e8f0
Huy ta <data@maxeagle.net>
2025-05-09 08:59:11 +0000

    ád

--- models/t3/t3_doi_soat_ads_pos.sql
+++ models/t3/t3_doi_soat_ads_pos.sql
@@ -1,35 +1,20 @@
-SELECT
-    a.date_start,
-    a.brand,
-    a.channel,
-    COALESCE(a.doanhThuShopeeSearch, 0) + COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0) AS totalAds,
-    COALESCE(SUM(p.total_price_after_sub_discount), 0) AS totalDoanhThuPos
-FROM {{ ref('t3_ads_total_with_tkqc') }} a
-FULL OUTER JOIN (
-    SELECT 
-        DATE(inserted_at) AS date,
-        brand,
-        CASE 
-            WHEN order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
-            WHEN order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
-            ELSE order_sources_name 
-        END AS channel,
-        SUM(total_price_after_sub_discount) AS total_price_after_sub_discount
-    FROM {{ ref('t1_pancake_pos_order_total') }}
-    GROUP BY 
-        DATE(inserted_at),
-        brand,
-        CASE 
-            WHEN order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
-            WHEN order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
-            ELSE order_sources_name 
-        END
-) p
-    ON a.date_start = p.date
-    AND a.brand = p.brand
-    AND a.channel = p.channel
+SELECT 
+    DATE(inserted_at) AS date,
+    brand,
+    CASE 
+        WHEN order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
+        WHEN order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
+        ELSE order_sources_name 
+    END AS channel,
+    COUNT(*) AS row_count,
+    SUM(total_price_after_sub_discount) AS total_pos_revenue
+FROM {{ ref('t1_pancake_pos_order_total') }}
 GROUP BY 
-    a.date_start,
-    a.brand,
-    a.channel,
-    (COALESCE(a.doanhThuShopeeSearch, 0) + COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0))
+    DATE(inserted_at),
+    brand,
+    CASE 
+        WHEN order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
+        WHEN order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
+        ELSE order_sources_name 
+    END
+ORDER BY date, brand, channel;
