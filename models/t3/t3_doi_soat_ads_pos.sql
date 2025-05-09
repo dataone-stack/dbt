bd228d1f48e5937111f17db52b55b857f5596ce9
Huy ta <data@maxeagle.net>
2025-05-09 08:57:49 +0000

    ádf

--- models/t3/t3_doi_soat_ads_pos.sql
+++ models/t3/t3_doi_soat_ads_pos.sql
@@ -3,17 +3,33 @@ SELECT
     a.brand,
     a.channel,
     COALESCE(a.doanhThuShopeeSearch, 0) + COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0) AS totalAds,
-    sum(p.total_price_after_sub_discount) AS totalDoanhThuPos
+    COALESCE(SUM(p.total_price_after_sub_discount), 0) AS totalDoanhThuPos
 FROM {{ ref('t3_ads_total_with_tkqc') }} a
-left JOIN {{ ref('t1_pancake_pos_order_total') }} p
-    ON a.date_start = DATE(p.inserted_at)
+FULL OUTER JOIN (
+    SELECT 
+        DATE(inserted_at) AS date,
+        brand,
+        CASE 
+            WHEN order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
+            WHEN order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
+            ELSE order_sources_name 
+        END AS channel,
+        SUM(total_price_after_sub_discount) AS total_price_after_sub_discount
+    FROM {{ ref('t1_pancake_pos_order_total') }}
+    GROUP BY 
+        DATE(inserted_at),
+        brand,
+        CASE 
+            WHEN order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
+            WHEN order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
+            ELSE order_sources_name 
+        END
+) p
+    ON a.date_start = p.date
     AND a.brand = p.brand
-    AND a.channel = CASE 
-                        WHEN p.order_sources_name = 'Ladipage Facebook' THEN 'Facebook'
-                        WHEN p.order_sources_name = 'Ladipage Tiktok' THEN 'Tiktok'
-                        ELSE p.order_sources_name 
-                    END
-group by a.date_start,
+    AND a.channel = p.channel
+GROUP BY 
+    a.date_start,
     a.brand,
     a.channel,
-    totalAds
+    (COALESCE(a.doanhThuShopeeSearch, 0) + COALESCE(a.doanhThuAds, 0) + COALESCE(a.doanhThuLadi, 0) + COALESCE(a.doanhThuGMVTiktok, 0))
