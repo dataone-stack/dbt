e8161e93432060c0b7ccd7a0a01181b908040ff7
Huy ta <data@maxeagle.net>
2025-05-11 07:36:43 +0000

    asd

--- models/t2/t2_ladipage_facebook_total.sql
+++ models/t2/t2_ladipage_facebook_total.sql
@@ -3,11 +3,11 @@ SELECT
   'Facebook' AS channel,
   od.brand,
   mar.staff,
-  sum(od.total_price_after_sub_discount) AS doanhThuLadi,
   mar.ma_nhan_vien AS id_staff,
   mar.manager AS manager,
+  sum(od.total_price_after_sub_discount) AS doanhThuLadi
 FROM {{ref('t1_pancake_pos_order_total')}} AS od
 left join {{ref("t1_marketer_facebook_total")}} as mar
-on od.JSON_VALUE(od.marketer, "$.name") = mar.marketer_name
+on od.json_value(od.marketer, "$.name") = mar.marketer_name
 WHERE od.marketer IS NOT NULl
 group by DATE(od.inserted_at),JSON_VALUE(od.marketer, "$.name"),id_staff,manager,od.brand
