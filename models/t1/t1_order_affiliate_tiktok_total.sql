1406218d112ebec4e5180401ff50c217d1016a97
Huy ta <data@maxeagle.net>
2025-05-11 07:55:38 +0000

    asdf

---
+++ models/t1/t1_order_affiliate_tiktok_total.sql
@@ -0,0 +1,9 @@
+select * from `chaching_tiktok_shop_dwh.tiktok_shop_chaching_brand_seller_affiliate_order`
+union all
+select * from `lyb_tiktok_shop_dwh.tiktok_shop_lyb_brand_seller_affiliate_order`
+union all
+select * from `lybcosmetic_tiktok_shop_dwh.tiktok_shop_lyb_cosmetic_seller_affiliate_order`
+union all
+select * from `ume_tiktok_shop_dwh.tiktok_shop_ume_viet_nam_seller_affiliate_order`
+union all
+select * from `ancung_tiktok_shop_dwh.tiktok_shop_an_cung_brand_seller_affiliate_order`
