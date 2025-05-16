72685ba70146d2080a1a27bf8432dd552705fdfb
Huy ta <data@maxeagle.net>
2025-05-16 11:00:15 +0000

    sadf

--- models/t2/t2_shopee_order_lines_toa.sql
+++ models/t2/t2_shopee_order_lines_toa.sql
@@ -55,7 +55,7 @@ sale_order_detail as (
     ord.shipping_carrier as ten_don_vi_van_chuyen,
     ord.ship_by_date as ngay_ship,
     ord.buyer_cancel_reason as ly_do_huy_don,
-    COALESCE(((sd.discounted_price) / ta.total_tong_tien_san_pham) * ord.day_to_ship,0)  as day_to_ship
+    COALESCE(((sd.discounted_price) / ta.total_tong_tien_san_pham) * ord.days_to_ship,0)  as day_to_ship
     
   from sale_detail as sd
   left join {{ref("t1_shopee_shop_order_detail_total")}} as ord
