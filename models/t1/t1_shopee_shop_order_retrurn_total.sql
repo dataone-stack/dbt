875ad27de7172e962ceff3bd04adc4afbfe80cd0
Huy ta <data@maxeagle.net>
2025-05-17 15:44:00 +0000

    asdf

--- models/t1/t1_shopee_shop_order_retrurn_total.sql
+++ models/t1/t1_shopee_shop_order_retrurn_total.sql
@@ -1,9 +1,13 @@
-select order_id,item,'Chaching' as brand from `chaching_shopee_shop_dwh.shopee_return_chaching_brand`
-union all
-select order_id,item,'LYB' as brand from lyb_shopee_shop_dwh.shopee_return_lyb_official`
-union all
-select order_id,item,'An Cung' as brand from `ancung_shopee_shop_dwh.shopee_return_ancung_brand`
-union all
-select order_id,item,'LYB Cosmetics' as brand from `lybcosmetic_shopee_shop_dwh.shopee_return_lyb_cosmetic`
-union all
-select order_id,item,'UME' as brand from `ume_shopee_shop_dwh.shopee_return_ume_viet_nam`
+create or replace table `crypto-arcade-453509-i8.dtm.t1_shopee_shop_order_retrurn_total`
+OPTIONS()
+as (
+  select order_id, item, 'Chaching' as brand from `chaching_shopee_shop_dwh.shopee_return_chaching_brand`
+  union all
+  select order_id, item, 'LYB' as brand from `lyb_shopee_shop_dwh.shopee_return_lyb_official`
+  union all
+  select order_id, item, 'An Cung' as brand from `ancung_shopee_shop_dwh.shopee_return_ancung_brand`
+  union all
+  select order_id, item, 'LYB Cosmetics' as brand from `lybcosmetic_shopee_shop_dwh.shopee_return_lyb_cosmetic`
+  union all
+  select order_id, item, 'UME' as brand from `ume_shopee_shop_dwh.shopee_return_ume_viet_nam`
+);
