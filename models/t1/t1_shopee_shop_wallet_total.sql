7faf0e66677e6653e3fc214cb0ad12710a490bff
Huy ta <data@maxeagle.net>
2025-05-20 02:45:49 +0000

    sadf

--- models/t1/t1_shopee_shop_wallet_total.sql
+++ models/t1/t1_shopee_shop_wallet_total.sql
@@ -1,1 +1,9 @@
-select * from `chaching_shopee_shop_dwh.shopee_payment_wallet_transaction_chaching_brand`
+select *,'Chaching' as brand from `chaching_shopee_shop_dwh.shopee_payment_wallet_transaction_chaching_brand`
+union all
+select *,'LYB' as brand from `lyb_shopee_shop_dwh.shopee_payment_wallet_transaction_lyb_official`
+union all
+select *,'LYB Cosmetics' as brand from `lybcosmetic_shopee_shop_dwh.shopee_payment_wallet_transaction_lyb_cosmetic`
+union all
+select *,'UME' as brand from `ume_shopee_shop_dwh.shopee_payment_wallet_transaction_ume_viet_nam`
+union all
+select *,'An Cung' as brand from `ancung_shopee_shop_dwh.shopee_payment_wallet_transaction_ancung_brand`
