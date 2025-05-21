61e19e5f1fc2ae99b1182e8667678dea31b19c2d
Huy ta <data@maxeagle.net>
2025-05-21 12:03:20 +0000

    dgf

--- models/t2/t2_shopee_order_lines_toa.sql
+++ models/t2/t2_shopee_order_lines_toa.sql
@@ -35,10 +35,10 @@ sale_detail AS (
     i.discounted_price,
     rd.update_time AS ngay_return,
     vi.create_time AS ngay_tien_ve_vi,
-    CASE 
-      WHEN DATE(rd.update_time)= DATE(vi.create_time) or rd.refund_amount = 0
+    CASE
+      WHEN DATE(rd.update_time) >= DATE(vi.create_time) or rd.refund_amount = 0
       THEN rd.so_tien_hoan_tra
-      ELSE rd.refund_amount
+      ELSE 0
     END AS so_tien_hoan_tra,
     (i.discounted_price) AS tong_tien_san_pham,
     (i.discounted_price / ta.total_tong_tien_san_pham) * detail.buyer_paid_shipping_fee AS phi_van_chuyen_nguoi_mua_tra,
