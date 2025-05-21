b8dce5ed6a04cf19833f74b2906351861f6ab079
Huy ta <data@maxeagle.net>
2025-05-21 09:07:42 +0000

    sfdg

--- models/t2/t2_shopee_order_lines_toa.sql
+++ models/t2/t2_shopee_order_lines_toa.sql
@@ -104,7 +104,7 @@ SELECT
   phi_thanh_toan,
   phi_hoa_hong_tiep_thi_lien_ket,
   tro_gia_tu_shopee,
-  ROUND(tong_tien_san_pham - so_tien_hoan_tra - phi_van_chuyen_nguoi_mua_tra -phi_van_chuyen_thuc_te + phi_van_chuyen_tro_gia_tu_shopee - tro_gia_tu_shopee - shopee_voucher - phi_co_dinh - phi_dich_vu - phi_thanh_toan - phi_hoa_hong_tiep_thi_lien_ket ) AS doanh_thu_don_hang_uoc_tinh,
+  ROUND(tong_tien_san_pham - so_tien_hoan_tra - phi_van_chuyen_nguoi_mua_tra -phi_van_chuyen_thuc_te + phi_van_chuyen_tro_gia_tu_shopee - tro_gia_tu_shopee - phi_co_dinh - phi_dich_vu - phi_thanh_toan - phi_hoa_hong_tiep_thi_lien_ket ) AS doanh_thu_don_hang_uoc_tinh,
   shopee_voucher,
   discount_from_coin,
   discount_from_voucher_seller,
