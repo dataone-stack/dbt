cb2d888ba4a171a3ecb36693b4ffe66c4495ec93
Huy ta <data@maxeagle.net>
2025-05-21 06:55:55 +0000

    asdf

--- models/t2/t2_shopee_order_tot.sql
+++ models/t2/t2_shopee_order_tot.sql
@@ -27,3 +27,4 @@ SELECT
 FROM {{ref("t1_shopee_shop_wallet_total")}} as vi
 LEFT JOIN {{ref("t2_shopee_order_toa")}} as ord 
 ON vi.order_id = ord.order_id AND vi.brand = ord.brand
+where vi.transaction_tab_type = 'wallet_order_income'
