8aac093801dc8cf4d58f178acc26de0d3e848dbe
Huy ta <data@maxeagle.net>
2025-05-22 06:48:29 +0000

    update

--- models/t1/t1_shopee_shop_order_detail_total.sql
+++ models/t1/t1_shopee_shop_order_detail_total.sql
@@ -1,9 +1,9 @@
-select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship,'Chaching' as brand from `chaching_shopee_shop_dwh.shopee_order_detail_chaching_brand`
+select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship, checkout_shipping_carrier,'Chaching' as brand from `chaching_shopee_shop_dwh.shopee_order_detail_chaching_brand`
 union all
-select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship,'LYB' as brand from `lyb_shopee_shop_dwh.shopee_order_detail_lyb_official`
+select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship, checkout_shipping_carrier,'LYB' as brand from `lyb_shopee_shop_dwh.shopee_order_detail_lyb_official`
 union all
-select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship,'An Cung' as brand from `ancung_shopee_shop_dwh.shopee_order_detail_ancung_brand`
+select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship, checkout_shipping_carrier,'An Cung' as brand from `ancung_shopee_shop_dwh.shopee_order_detail_ancung_brand`
 union all
-select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship,'LYB Cosmetics' as brand from `lybcosmetic_shopee_shop_dwh.shopee_order_detail_lyb_cosmetic`
+select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship, checkout_shipping_carrier,'LYB Cosmetics' as brand from `lybcosmetic_shopee_shop_dwh.shopee_order_detail_lyb_cosmetic`
 union all
-select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship,'UME' as brand from `ume_shopee_shop_dwh.shopee_order_detail_ume_viet_nam`
+select order_id,total_amount,create_time,order_status,payment_method,shipping_carrier,ship_by_date,buyer_cancel_reason,days_to_ship, checkout_shipping_carrier,'UME' as brand from `ume_shopee_shop_dwh.shopee_order_detail_ume_viet_nam`
