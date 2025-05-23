c5fa3f96598bfe865b6573af06155386a8d2dd05
Huy ta <data@maxeagle.net>
2025-05-23 03:56:51 +0000

    adsf

--- models/t2/t2_facebook_order_lines_total.sql
+++ models/t2/t2_facebook_order_lines_total.sql
@@ -3,6 +3,6 @@ select
     ord.brand,
     i.quantity
 from {{ref("t1_pancake_pos_order_total")}} as ord,
-UNNEST(item) AS i
+UNNEST(items) AS i
 where ord.order_sources_name in ('Facebook','Ladipage Facebook')
 
