48dd6e3f5ec5fd049618cec4f4d5838d14f56efc
Huy ta <data@maxeagle.net>
2025-05-23 04:02:06 +0000

    asdf

--- models/t2/t2_facebook_order_lines_total.sql
+++ models/t2/t2_facebook_order_lines_total.sql
@@ -1,8 +1,12 @@
-select 
+SELECT
     ord.id,
     ord.brand,
-    SAFE_CAST(i.quantity AS INT64) AS quantity
-from {{ref("t1_pancake_pos_order_total")}} as ord,
+    SAFE_CAST(
+        CASE
+            WHEN i.quantity IS NOT NULL AND TRIM(i.quantity) REGEXP '^[0-9]+$' THEN i.quantity
+            ELSE NULL
+        END AS INT64
+    ) AS quantity
+FROM {{ref("t1_pancake_pos_order_total")}} AS ord,
 UNNEST(items) AS i
-where ord.order_sources_name in ('Facebook','Ladipage Facebook')
-
+WHERE ord.order_sources_name IN ('Facebook', 'Ladipage Facebook')
