368a158ba4a4f48239325ce2b3e0b14728baf723
data@maxeagle.net <data@maxeagle.net>
2025-05-27 07:30:15 +0000

    asdf

--- models/t1/t1_tiktok_affiliate_order_total.sql
+++ models/t1/t1_tiktok_affiliate_order_total.sql
@@ -1,9 +1,9 @@
-select *, 'from `chaching_tiktok_shop_dwh.tiktok_shop_chaching_brand_seller_affiliate_order`
+select *, 'chaching' as brand from `chaching_tiktok_shop_dwh.tiktok_shop_chaching_brand_seller_affiliate_order`
 union all
-select * from `lyb_tiktok_shop_dwh.tiktok_shop_lyb_brand_seller_affiliate_order`,
+select *,'lyb' AS brand from `lyb_tiktok_shop_dwh.tiktok_shop_lyb_brand_seller_affiliate_order`,
 union all
-select * from `ancung_tiktok_shop_dwh.tiktok_shop_an_cung_brand_seller_affiliate_order`
+select *,'an_cung' AS brand from `ancung_tiktok_shop_dwh.tiktok_shop_an_cung_brand_seller_affiliate_order`
 union all
-select * from `lybcosmetic_tiktok_shop_dwh.tiktok_shop_lyb_cosmetic_seller_affiliate_order`
+select *,'lybcosmetic' AS brand from `lybcosmetic_tiktok_shop_dwh.tiktok_shop_lyb_cosmetic_seller_affiliate_order`
 union all
-select * from `ume_tiktok_shop_dwh.tiktok_shop_ume_viet_nam_seller_affiliate_order`
+select *,'ume' AS brand from `ume_tiktok_shop_dwh.tiktok_shop_ume_viet_nam_seller_affiliate_order`
