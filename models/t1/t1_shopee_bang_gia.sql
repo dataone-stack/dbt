bd445c7f00e90e80a62c06cfefbb3d8705fa2f65
Huy ta <data@maxeagle.net>
2025-05-27 11:12:13 +0000

    update

--- models/t1/t1_shopee_bang_gia.sql
+++
@@ -1,9 +0,0 @@
-select *,'Chaching' as brand from `google_sheet.bang_gia_chaching`
-union all
-select *,'LYB' as brand from `google_sheet.bang_gia_lybfaction`
-union all
-select *,'LYB Cosmetics' as brand from `google_sheet.bang_gia_lybcosmetic`
-union all
-select *,'UME' as brand from `google_sheet.bang_gia_ume`
-union all
-select *,'An Cung' as brand from `google_sheet.bang_gia_ancung`
