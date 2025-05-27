9c71e52bcefa2710e71c5e0f85f8bdcf0d08c791
data@maxeagle.net <data@maxeagle.net>
2025-05-27 07:35:32 +0000

    asdf

--- models/t1/t1_tiktok_affiliate_order_total.sql
+++ models/t1/t1_tiktok_affiliate_order_total.sql
@@ -1,6 +1,6 @@
 select *, 'chaching' as brand from `chaching_tiktok_shop_dwh.tiktok_shop_chaching_brand_seller_affiliate_order`
 union all
-select *,'lyb' AS brand from `lyb_tiktok_shop_dwh.tiktok_shop_lyb_brand_seller_affiliate_order`,
+select *,'lyb' AS brand from `lyb_tiktok_shop_dwh.tiktok_shop_lyb_brand_seller_affiliate_order`
 union all
 select *,'an_cung' AS brand from `ancung_tiktok_shop_dwh.tiktok_shop_an_cung_brand_seller_affiliate_order`
 union all
