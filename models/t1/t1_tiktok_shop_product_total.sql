116347649249c25c1d8bbbedd40d9464c30e9f8c
Huy ta <data@maxeagle.net>
2025-05-27 08:39:54 +0000

    dsfg

---
+++ models/t1/t1_tiktok_shop_product_total.sql
@@ -0,0 +1,9 @@
+select *,'chaching' as brand from `chaching_tiktok_shop_dwh.tiktok_shop_chaching_brand_product`
+union all
+select *,'lyb' AS brand from `lyb_tiktok_shop_dwh.tiktok_shop_lyb_brand_product`
+union all
+select *,'an_cung' AS brand from `ancung_tiktok_shop_dwh.tiktok_shop_an_cung_brand_product`
+union all
+select *,'lybcosmetic' AS brand from `lybcosmetic_tiktok_shop_dwh.tiktok_shop_lyb_cosmetic_product`
+union all
+select * ,'ume' AS brand from `ume_tiktok_shop_dwh.tiktok_shop_ume_viet_nam_product`
