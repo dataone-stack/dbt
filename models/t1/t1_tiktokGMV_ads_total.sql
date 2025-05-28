43b5d6c80c30f2f4b6dcf393c08fe7984ccf2afa
Huy ta <data@maxeagle.net>
2025-05-28 11:55:33 +0000

    a

---
+++ models/t1/t1_tiktokGMV_ads_total.sql
@@ -0,0 +1,9 @@
+select * from tiktok_ads_dwh.chaching_tiktok_gmvMax_dwh
+union all
+select * from tiktok_ads_dwh.lyb_tiktok_gmvMax_dwh
+union all
+select * from tiktok_ads_dwh.ancung_tiktok_gmvMax_dwh
+union all
+select * from tiktok_ads_dwh.ume_tiktok_gmvMax_dwh
+union all
+select * from tiktok_ads_dwh.lybcosmetic_tiktok_gmvMax_dwh
