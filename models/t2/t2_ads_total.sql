9e2236058d623c081cf9766acf3d808b59c6f690
Huy ta <data@maxeagle.net>
2025-05-29 02:21:51 +0000

    asdf

--- models/t2/t2_ads_total.sql
+++ models/t2/t2_ads_total.sql
@@ -11,10 +11,25 @@ SELECT date(date_start) as date_start,account_id,spend,COALESCE(
       ) AS FLOAT64
     ),
     0
-  ) AS doanhThuAds FROM  {{ ref('t1_facebook_ads_total')}}
+  ) AS doanhThuAds,
+  COALESCE(
+    CAST(
+      JSON_VALUE(
+        (
+          SELECT value
+          FROM UNNEST(actions) AS value
+          WHERE JSON_VALUE(value, '$.action_type') = 'onsite_conversion.purchase'
+          LIMIT 1
+        ),
+        '$.value'
+      ) AS INT
+    ),
+    0
+  ) AS purchase,
+  cpc,cpm FROM  {{ ref('t1_facebook_ads_total')}}
 union all
-select date(stat_time_day) as date_start ,account_id,spend,cast(total_onsite_shopping_value as FLOAT64) as doanhThuAds From {{ref("t1_tiktok_ads_total")}}
+select date(stat_time_day) as date_start ,account_id,spend,cast(total_onsite_shopping_value as FLOAT64),onsite_shopping as purchase,cpc,cpm as doanhThuAds From {{ref("t1_tiktok_ads_total")}}
 union all
-select date(stat_time_day) as date_start ,account_id,spend, 0 as doanhThuAds From {{ref("t1_tiktokGMV_ads_total")}}
+select date(stat_time_day) as date_start ,account_id,spend, 0 as doanhThuAds,0 as purchase,0 as cpc, 0 as cpm From {{ref("t1_tiktokGMV_ads_total")}}
 union all
-select date(date) as date_start , cast(idtkqc as int64) as account_id,expense as spend, broad_gmv as doanhThuAds From {{ref("t1_shopee_ads_total")}}l
+select date(date) as date_start , cast(idtkqc as int64) as account_id,expense as spend, broad_gmv as doanhThuAds, 0 as purchase, 0 as cpc, 0 as cpm From {{ref("t1_shopee_ads_total")}}l
