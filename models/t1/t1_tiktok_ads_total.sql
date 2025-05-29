dea5c64ff4c69349b92144d4aadb7d380901e0cf
Huy ta <data@maxeagle.net>
2025-05-29 02:23:43 +0000

    asdf

--- models/t1/t1_tiktok_ads_total.sql
+++ models/t1/t1_tiktok_ads_total.sql
@@ -1,9 +1,9 @@
-SELECT total_onsite_shopping_value,spend,account_id,stat_time_day FROM tiktok_ads_dwh.chaching_tiktok_ads_dwh
+SELECT onsite_shopping,total_onsite_shopping_value,spend,account_id,stat_time_day FROM tiktok_ads_dwh.chaching_tiktok_ads_dwh
 UNION ALL
-SELECT total_onsite_shopping_value,spend,account_id,stat_time_day FROM tiktok_ads_dwh.lyb_tiktok_ads_dwh
+SELECT onsite_shopping ,total_onsite_shopping_value,spend,account_id,stat_time_day FROM tiktok_ads_dwh.lyb_tiktok_ads_dwh
 UNION ALL
-SELECT total_onsite_shopping_value,spend,account_id,stat_time_day FROM tiktok_ads_dwh.ume_tiktok_ads_dwh
+SELECT onsite_shopping ,total_onsite_shopping_value,spend,account_id,stat_time_day FROM tiktok_ads_dwh.ume_tiktok_ads_dwh
 union all
-SELECT safe_cast(total_onsite_shopping_value as int64),spend,account_id,stat_time_day from tiktok_ads_dwh.ancung_tiktok_ads_dwh
+SELECT onsite_shopping ,safe_cast(total_onsite_shopping_value as int64),spend,account_id,stat_time_day from tiktok_ads_dwh.ancung_tiktok_ads_dwh
 union all
-SELECT total_onsite_shopping_value,spend,account_id,stat_time_day from tiktok_ads_dwh.lybcosmetic_tiktok_ads_dwh
+SELECT onsite_shopping ,total_onsite_shopping_value,spend,account_id,stat_time_day from tiktok_ads_dwh.lybcosmetic_tiktok_ads_dwh
