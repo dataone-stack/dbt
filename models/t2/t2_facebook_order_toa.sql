832008ba37a8548486004cf91312ea3aa0019c91
Huy ta <data@maxeagle.net>
2025-05-30 02:12:26 +0000

    sadf

--- models/t2/t2_facebook_order_toa.sql
+++ models/t2/t2_facebook_order_toa.sql
@@ -6,7 +6,6 @@ select
   sum(gia_goc) as gia_goc,
   sum(khuyen_mai_dong_gia) as khuyen_mai_dong_gia, 
   sum(giam_gia_don_hang) as giam_gia_don_hang,
-  sum(phi_van_chuyen) as phi_van_chuyen,
   sum(tong_tien_sau_giam_gia) as tong_tien_sau_giam_gia,
   sum(cod) as cod,
   sum(tra_truoc) as tra_truoc,
