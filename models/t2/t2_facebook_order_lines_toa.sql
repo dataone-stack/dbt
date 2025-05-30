97fd9bf2ed09825aa32f320cdd82a5fdfe00a4de
Huy ta <data@maxeagle.net>
2025-05-30 02:49:24 +0000

    asdf

--- models/t2/t2_facebook_order_lines_toa.sql
+++ models/t2/t2_facebook_order_lines_toa.sql
@@ -57,8 +57,8 @@ select
   gia_goc,
   khuyen_mai_dong_gia,
   giam_gia_don_hang,
-  gia_goc - khuyen_mai_dong_gia - giam_gia_don_hang + phi_van_chuyen as tong_tien_sau_giam_gia,
-  gia_goc - khuyen_mai_dong_gia - giam_gia_don_hang + phi_van_chuyen - tra_truoc as cod,
+  gia_goc * so_luong   - khuyen_mai_dong_gia - giam_gia_don_hang + phi_van_chuyen as tong_tien_sau_giam_gia,
+  gia_goc * so_luong - khuyen_mai_dong_gia - giam_gia_don_hang + phi_van_chuyen - tra_truoc as cod,
   tra_truoc,
   cuoc_vc,
   phi_van_chuyen
