ceecdc3b7104bf260fc8d0ab9816d68e8d243da3
Huy ta <data@maxeagle.net>
2025-06-03 10:59:15 +0000

    dsaf

--- models/t1/t1_facebook_ads_total.sql
+++ models/t1/t1_facebook_ads_total.sql
@@ -5,8 +5,5 @@ UNION ALL
 SELECT * FROM lyb_facebook_ads_dwh.facebook_ads_ads_insights
 union all
 SELECT * FROM ancung_facebook_ads_dwh.facebook_ads_ads_insights
-union all
-SELECT * FROM me_qa_minh_facebook_ads_dwh.facebook_ads_ads_insights
-union all
-SELECT * FROM me_qa_phuong_facebook_ads_dwh.facebook_ads_ads_insights
+
 
