cf398593e3c2df156ced17c3d11ad1c3219fc859
Huy ta <data@maxeagle.net>
2025-06-05 09:02:05 +0000

    df

--- models/t3/t3_ads_total_with_tkqc.sql
+++ models/t3/t3_ads_total_with_tkqc.sql
@@ -24,41 +24,6 @@ WITH ads_total_with_tkqc AS (
         tkqc.brand,
         tkqc.channel,
         ads.revenue_type
-),
-
-ads_ladipageFacebook_total_with_tkqc AS (
-    SELECT 
-        ads.date_start,
-        ads.idtkqc,
-        ads.nametkqc,
-        ads.ma_nhan_vien,
-        ads.staff,
-        ads.manager,
-        ads.brand,
-        ads.channel,
-        ads.chiPhiAds,
-        ads.doanhThuAds,
-        CASE 
-            WHEN ROW_NUMBER() OVER (
-                PARTITION BY ads.date_start, ads.ma_nhan_vien, ads.manager, ads.brand, ads.channel 
-                ORDER BY ladi.date_insert
-            ) = 1 THEN ladi.doanhThuLadi 
-            ELSE 0 
-        END AS doanhThuLadi,
-        CASE 
-            WHEN ROW_NUMBER() OVER (
-                PARTITION BY ads.date_start, ads.ma_nhan_vien, ads.manager, ads.brand, ads.channel 
-                ORDER BY ladi.date_insert
-            ) = 1 THEN 'Ladipage'
-            ELSE ads.revenue_type
-        END AS revenue_type
-    FROM ads_total_with_tkqc AS ads
-    LEFT JOIN {{ ref('t2_ladipage_facebook_total') }} AS ladi
-        ON ads.date_start = ladi.date_insert 
-        AND ads.ma_nhan_vien = ladi.id_staff 
-        AND ads.manager = ladi.manager 
-        AND ads.brand = ladi.brand 
-        AND ads.channel = ladi.channel
 )
 
 SELECT
@@ -72,11 +37,21 @@ SELECT
     ads.channel,
     ads.chiPhiAds,
     ads.doanhThuAds,
-    ads.doanhThuLadi,
+    COALESCE(ladi.doanhThuLadi, 0) AS doanhThuLadi,
     gmv.gross_revenue AS doanhThuGMVTiktok,
     shopeeSearch.doanhThuAds AS doanhThuShopeeSearch,
-    ads.revenue_type AS loaiDoanhThu
-FROM ads_ladipageFacebook_total_with_tkqc AS ads 
+    -- Nếu có doanhThuLadi > 0 thì revenue_type = 'Ladipage', ngược lại lấy từ ads
+    CASE 
+        WHEN ladi.doanhThuLadi IS NOT NULL AND ladi.doanhThuLadi > 0 THEN 'Ladipage'
+        ELSE ads.revenue_type
+    END AS loaiDoanhThu
+FROM ads_total_with_tkqc AS ads
+LEFT JOIN {{ ref('t2_ladipage_facebook_total') }} AS ladi
+    ON ads.date_start = ladi.date_insert 
+    AND ads.ma_nhan_vien = ladi.id_staff 
+    AND ads.manager = ladi.manager 
+    AND ads.brand = ladi.brand 
+    AND ads.channel = ladi.channel
 LEFT JOIN {{ ref('t1_tiktokGMV_ads_total') }} AS gmv 
     ON DATE(DATETIME_ADD(DATETIME(gmv.stat_time_day), INTERVAL 7 HOUR)) = ads.date_start 
     AND CAST(gmv.account_id AS STRING) = CAST(ads.idtkqc AS STRING)
