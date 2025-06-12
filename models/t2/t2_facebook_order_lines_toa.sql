196aa4ff339f45afd1c0d799d8a69f40b093fc75
Huy ta <data@maxeagle.net>
2025-06-12 10:29:22 +0000

    sdfds

--- models/t2/t2_facebook_order_lines_toa.sql
+++ models/t2/t2_facebook_order_lines_toa.sql
@@ -55,7 +55,7 @@ order_line as (
   from {{ref("t1_pancake_pos_order_total")}} as ord,
   unnest (items) as item
   left join total_price as tt on tt.id = ord.id and tt.brand = ord.brand
-  left join {{ref("t1_bang_gia_san_pham")}} as mapBangGia on json_value(item, '$.variation_info.display_id') = mapBangGia.ma_sku
+  left join {{ref("t1_bang_gia_san_pham")}} as mapBangGia on json_value(item, '$.variation_info.display_id') = mapBangGia.ma_sku and ord.brand = mapBangGia.brand
   where ord.order_sources_name in ('Facebook','Ladipage Facebook','Webcake','') and ord.status_name not in ('removed')
 )
 
