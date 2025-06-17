b7b2e0404580bd1065e1f92e71a9a7d8ef97bea6
Huy ta <data@maxeagle.net>
2025-06-17 04:29:14 +0000

    dfg

--- models/t1/t1_facebook_ads_total.sql
+++ models/t1/t1_facebook_ads_total.sql
@@ -1,6 +1,4 @@
--- select * from `me_qa_phuong_facebook_ads_dwh.facebook_ads_ads_insights_default`
--- union all
--- select * from `me_qa_minh_facebook_ads_dwh.facebook_ads_ads_insights_default`
+
 
 select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
 union all
