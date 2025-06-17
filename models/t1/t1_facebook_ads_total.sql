a4e8c5cfd87213fced6479a0cabeedb7ec51352e
Huy ta <data@maxeagle.net>
2025-06-17 08:26:24 +0000

    update

--- models/t1/t1_facebook_ads_total.sql
+++ models/t1/t1_facebook_ads_total.sql
@@ -1,11 +1,11 @@
+select * from `tai_nguyen_facebook_ads_dwh.facebook_ads_ads_insights_default`
 
-
-select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
-union all
-select * from `chaching_facebook_ads_dwh.facebook_ads_ads_insights_default`
-union all
-select * from `ume_facebook_ads_dwh.facebook_ads_ads_insights_default`
-union all
-select * from `lyb_facebook_ads_dwh.facebook_ads_ads_insights_default`
-union all
-select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
+-- select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
+-- union all
+-- select * from `chaching_facebook_ads_dwh.facebook_ads_ads_insights_default`
+-- union all
+-- select * from `ume_facebook_ads_dwh.facebook_ads_ads_insights_default`
+-- union all
+-- select * from `lyb_facebook_ads_dwh.facebook_ads_ads_insights_default`
+-- union all
+-- select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
