f3f9641d3ba0c9feb4afa6eb891275be50a47186
Huy ta <data@maxeagle.net>
2025-06-18 15:07:11 +0000

    update

--- models/t1/t1_facebook_ads_total.sql
+++ models/t1/t1_facebook_ads_total.sql
@@ -18,3 +18,9 @@ union all
 select * from `lyb_facebook_ads_dwh.facebook_ads_ads_insights_default`
 union all
 select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
+union all
+select * from `team_maxeagle_facebook_ads_3_dwh.facebook_ads_ads_insights_default`
+union all
+select * from `team_maxeagle_facebook_ads_2_dwh.facebook_ads_ads_insights_default` 
+union all
+select * from `team_maxeagle_facebook_ads_1_dwh.facebook_ads_ads_insights_default` 
