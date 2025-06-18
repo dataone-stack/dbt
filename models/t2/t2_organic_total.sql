07bcc1e58f80e27b5b76bce22fb4bb71e3099a54
Huy ta <data@maxeagle.net>
2025-06-18 07:08:15 +0000

    d

--- models/t2/t2_organic_total.sql
+++ models/t2/t2_organic_total.sql
@@ -1,8 +1,8 @@
 with pos_total as(
-select date(datetime_add(inserted_at, INTERVAL 7 hour)) as date_insert, 'Facebook' as channel,brand, sum(total_price_after_sub_discount) as doanhthu
-from {{ref("t1_pancake_pos_order_total")}}
-where order_sources_name in ('Facebook') and marketer is null and brand != 'UME'
-group by brand,date(datetime_add(inserted_at, INTERVAL 7 hour))
+    select date(datetime_add(inserted_at, INTERVAL 7 hour)) as date_insert, 'Facebook' as channel,brand, sum(total_price_after_sub_discount) as doanhthu
+    from {{ref("t1_pancake_pos_order_total")}}
+    where order_sources_name in ('Facebook') and marketer is null and brand != 'UME'
+    group by brand,date(datetime_add(inserted_at, INTERVAL 7 hour))
 ),
 
 doanh_thu_ads_total as (
@@ -34,16 +34,16 @@ group by
 ),
 
 organic as (
-select
-  ads.date_start,
-  ads.channel,
-  ads.brand,
-  pos.doanhthu - ads.doanhThuAds as doanhThuOrganic
-from pos_total pos 
-left join doanh_thu_ads_total ads on pos.date_insert = ads.date_start and pos.channel = ads.channel and pos.brand = ads.brand
+    select
+    ads.date_start,
+    ads.channel,
+    ads.brand,
+    pos.doanhthu - ads.doanhThuAds as doanhThuOrganic
+    from pos_total pos 
+    left join doanh_thu_ads_total ads on pos.date_insert = ads.date_start and pos.channel = ads.channel and pos.brand = ads.brand
 )
 
-  SELECT
+    SELECT
     DATE(adstot.date_start) AS date_start,
     tk.channel,
     tk.brand,
@@ -72,19 +72,19 @@ left join doanh_thu_ads_total ads on pos.date_insert = ads.date_start and pos.ch
             SUM(adstot.doanhThuAds)
         ) AS INT64
     ) AS doanhThuOrganic
-  FROM {{ref("t1_facebook_ads_total")}} fb 
-  LEFT JOIN {{ref("t1_tkqc")}} tk ON cast(fb.account_id as string) = tk.idtkqc
-  left join doanh_thu_ads_total adstot on fb.date_start =  adstot.date_start and tk.brand = adstot.brand and tk.channel = adstot.channel
-  left join organic org on fb.date_start =  org.date_start and tk.brand = org.brand and tk.channel = org.channel
-  group by  
-      adstot.date_start,
-      tk.channel,
-      tk.brand,
-      tk.ma_nhan_vien,
-      tk.ma_quan_ly,
-      tk.staff,
-      tk.manager
-      
+    FROM {{ref("t1_facebook_ads_total")}} fb 
+    LEFT JOIN {{ref("t1_tkqc")}} tk ON cast(fb.account_id as string) = tk.idtkqc
+    left join doanh_thu_ads_total adstot on fb.date_start =  adstot.date_start and tk.brand = adstot.brand and tk.channel = adstot.channel
+    left join organic org on fb.date_start =  org.date_start and tk.brand = org.brand and tk.channel = org.channel
+    group by  
+        adstot.date_start,
+        tk.channel,
+        tk.brand,
+        tk.ma_nhan_vien,
+        tk.ma_quan_ly,
+        tk.staff,
+        tk.manager
+        
 
 
 
