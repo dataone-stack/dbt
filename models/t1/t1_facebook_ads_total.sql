fd11b6ce922127a766033e28fdba365faa3a956b
Huy ta <data@maxeagle.net>
2025-06-18 04:35:06 +0000

    update

--- models/t1/t1_facebook_ads_total.sql
+++ models/t1/t1_facebook_ads_total.sql
@@ -1,20 +1,20 @@
-SELECT * FROM `me_qa_phuong_facebook_ads_dwh.facebook_ads_ads_insights_default`
-UNION ALL
-SELECT * FROM `me_qa_minh_facebook_ads_dwh.facebook_ads_ads_insights_default` m
-WHERE NOT EXISTS (
-  SELECT 1 FROM `me_qa_phuong_facebook_ads_dwh.facebook_ads_ads_insights_default` p
-  WHERE p.account_id = m.account_id
-)
+-- SELECT * FROM `me_qa_phuong_facebook_ads_dwh.facebook_ads_ads_insights_default`
+-- UNION ALL
+-- SELECT * FROM `me_qa_minh_facebook_ads_dwh.facebook_ads_ads_insights_default` m
+-- WHERE NOT EXISTS (
+--   SELECT 1 FROM `me_qa_phuong_facebook_ads_dwh.facebook_ads_ads_insights_default` p
+--   WHERE p.account_id = m.account_id
+-- )
 
 
 --select * from `tai_nguyen_facebook_ads_dwh.facebook_ads_ads_insights_default`
 
--- select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
--- union all
--- select * from `chaching_facebook_ads_dwh.facebook_ads_ads_insights_default`
--- union all
--- select * from `ume_facebook_ads_dwh.facebook_ads_ads_insights_default`
--- union all
--- select * from `lyb_facebook_ads_dwh.facebook_ads_ads_insights_default`
--- union all
--- select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
+select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
+union all
+select * from `chaching_facebook_ads_dwh.facebook_ads_ads_insights_default`
+union all
+select * from `ume_facebook_ads_dwh.facebook_ads_ads_insights_default`
+union all
+select * from `lyb_facebook_ads_dwh.facebook_ads_ads_insights_default`
+union all
+select * from `ancung_facebook_ads_dwh.facebook_ads_ads_insights_default`
