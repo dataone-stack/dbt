a2b328d8ca712f8cbf1e8f8f92e1a0a616f4f11b
Huy ta <data@maxeagle.net>
2025-06-25 06:38:30 +0000

    df

--- models/t2/t2_ladipage_facebook_total.sql
+++ models/t2/t2_ladipage_facebook_total.sql
@@ -18,14 +18,3 @@ GROUP BY
   mar.ma_nhan_vien,
   mar.ma_quan_ly,
   od.brand
-
---   SELECT 
---   od.brand,
---   SUM(od.total_price_after_sub_discount) AS doanhThuLadi
--- FROM crypto-arcade-453509-i8.dtm.t1_pancake_pos_order_total AS od
--- LEFT JOIN crypto-arcade-453509-i8.dtm.t1_marketer_facebook_total AS mar
---   ON JSON_VALUE(od.marketer, '$.name') = mar.marketer_name
--- WHERE od.marketer IS NOT NULL 
---   AND od.order_sources_name IN ('Facebook', 'Ladipage Facebook', 'Webcake')
---   AND  od.status_name NOT IN ('new', 'removed')
---   and DATE(DATE_ADD(od.inserted_at, INTERVAL 7 HOUR)) between '2025-06-01' and '2025-06-24'
