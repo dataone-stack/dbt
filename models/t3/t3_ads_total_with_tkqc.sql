7c2d3feaf2fb03deda225169196839ec354b1c94
Huy ta <data@maxeagle.net>
2025-06-25 04:25:32 +0000

    i

--- models/t3/t3_ads_total_with_tkqc.sql
+++ models/t3/t3_ads_total_with_tkqc.sql
@@ -91,9 +91,3 @@ ads.revenue_type AS loaiDoanhThu,
 ads.company,
 ads.ben_thue
 FROM ads_ladipageFacebook_total_with_tkqc AS ads
--- LEFT JOIN {{ ref('t1_tiktokGMV_ads_total') }} AS gmv
--- ON DATE(DATETIME_ADD(DATETIME(gmv.stat_time_day), INTERVAL 7 HOUR)) = ads.date_start
--- AND CAST(gmv.account_id AS STRING) = CAST(ads.idtkqc AS STRING)
--- LEFT JOIN {{ ref('t1_shopee_search_ads_total') }} AS shopeeSearch
--- ON shopeeSearch.date_start = ads.date_start
--- AND CAST(shopeeSearch.account_id AS STRING) = CAST(ads.idtkqc AS STRING)
