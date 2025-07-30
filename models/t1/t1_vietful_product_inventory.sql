7be898f1752ea15c59f97c8f67c05f8f9f294129
Huy ta <data@maxeagle.net>
2025-07-30 02:52:50 +0000

    update

---
+++ models/t1/t1_vietful_product_inventory.sql
@@ -0,0 +1,5 @@
+select * from `chaching_vietful_dwh.vietful_product_inventory_lyb`
+UNION ALL
+select * from `lyb_vietful_dwh.vietful_product_inventory_lyb`
+UNION ALL
+select * from `ume_vietful_dwh.vietful_product_inventory_lyb`
