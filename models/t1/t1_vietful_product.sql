237717b2b1f71278bae9527eb4cdfd7452741d01
Huy ta <data@maxeagle.net>
2025-08-08 10:22:14 +0000

    sad

--- models/t1/t1_vietful_product.sql
+++
@@ -1,5 +0,0 @@
-select * from `chaching_vietful_dwh.vietful_product_lyb`
-UNION ALL
-select * from `lyb_vietful_dwh.vietful_product_lyb`
-UNION ALL
-select * from `ume_vietful_dwh.vietful_product_lyb`
--- models/t1/t1_vietful_product_inventory.sql
+++ models/t1/t1_vietful_product_inventory.sql
@@ -1,6 +1,8 @@
-select * from `chaching_vietful_dwh.vietful_product_inventory_lyb`
-UNION ALL
-select * from `lyb_vietful_dwh.vietful_product_inventory_lyb`
+select warehouse_code, partner_sku, sku, unit_code, condition_type_code, date_record, physical_qty, available_qty, pending_in_qty, pending_out_qty, 	
+freeze_qty, 'Chaching' as brand  from `chaching_vietful_dwh.vietful_product_inventory_lyb`
 UNION ALL
 select warehouse_code, partner_sku, sku, unit_code, condition_type_code, date_record, physical_qty, available_qty, pending_in_qty, pending_out_qty, 	
-freeze_qty from `ume_vietful_dwh.vietful_product_inventory_lyb`
+freeze_qty, 'LYB' as brand from `lyb_vietful_dwh.vietful_product_inventory_lyb`
+UNION ALL
+select warehouse_code, partner_sku, sku, unit_code, condition_type_code, date_record, physical_qty, available_qty, pending_in_qty, pending_out_qty, 	
+freeze_qty, 'UME' as brand from `ume_vietful_dwh.vietful_product_inventory_lyb`
