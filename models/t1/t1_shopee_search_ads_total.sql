6cb87c139da8411e0006df953558fedd90cb6526
Huy ta <data@maxeagle.net>
2025-08-11 03:11:05 +0000

    dsaf

--- models/t1/t1_shopee_search_ads_total.sql
+++ models/t1/t1_shopee_search_ads_total.sql
@@ -1,1 +1,12 @@
-select * from google_sheet.shopee_search_temp
+SELECT *
+FROM google_sheet.shopee_search_temp
+--loại bỏ các dòng trống trong ggsheet
+WHERE TRIM(CONCAT(
+    COALESCE(CAST(account_id AS STRING), ''), 
+    COALESCE(CAST(doanhThuAds AS STRING), ''), 
+    COALESCE(CAST(chiphi AS STRING), ''), 
+    COALESCE(CAST(date_start AS STRING), '')
+)) <> ''
+
+
+-- select * from google_sheet.shopee_search_temp
