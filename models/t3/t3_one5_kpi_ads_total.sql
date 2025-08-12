6ed0bb8bdead93431b73603ad9e63c8e59f3ab7b
Huy ta <data@maxeagle.net>
2025-08-12 07:22:54 +0000

    update name one5

---
+++ models/t3/t3_one5_kpi_ads_total.sql
@@ -0,0 +1,39 @@
+WITH ads_total AS (
+  SELECT 
+    brand,
+    channel,
+    date_start,
+    ma_nhan_vien,
+    staff,
+    ma_quan_ly,
+    manager,
+    SUM(doanhThuads + doanhThuLadi) AS DoanhThuAds,
+    SUM(chiPhiAds) AS chiPhiAds,
+    ROUND(SAFE_DIVIDE(SUM(chiPhiAds), SUM(doanhThuads + doanhThuLadi)), 4) AS cir,
+
+  FROM {{ref('t3_ads_total_with_tkqc')}}
+  WHERE company = 'One5'
+  GROUP BY
+    brand,
+    channel,
+    date_start,
+    ma_nhan_vien,
+    staff,
+    ma_quan_ly,
+    manager
+)
+
+SELECT 
+  a.*,
+  b.month as kpi_month,
+  b.year as kpi_year,
+  b.revenue_target,
+  b.cir_target
+FROM ads_total a
+LEFT JOIN {{ref('t1_kpi_ads_total')}} b
+  ON a.brand = b.brand
+  AND a.channel = b.channel
+  AND a.ma_nhan_vien = b.ma_nhan_vien
+  AND a.ma_quan_ly = b.manager_code
+  AND EXTRACT(MONTH FROM a.date_start) = b.month
+  AND EXTRACT(YEAR FROM a.date_start) = b.year
