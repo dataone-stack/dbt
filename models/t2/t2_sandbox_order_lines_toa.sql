e84723bfbaa194d3689631805a45a1d1bd2466bb
Huy ta <data@maxeagle.net>
2025-08-19 03:17:44 +0000

    dfas

--- models/t2/t2_sandbox_order_lines_toa.sql
+++ models/t2/t2_sandbox_order_lines_toa.sql
@@ -115,8 +115,8 @@ orderline AS (
             )
         END,0) AS chiet_khau,
 
-        -- total_discount_product đang update
-        --dt.discount AS giam_gia_san_pham,
+        
+        0 AS giam_gia_san_pham,
 
         ROUND(SAFE_DIVIDE(dt.quantity * dt.price, NULLIF(ord.total_price, 0)) * ord.total_cod, 0) AS gia_dich_vu_vc,
         ROUND(SAFE_DIVIDE(dt.quantity * dt.price, NULLIF(ord.total_price, 0)) * 
@@ -243,12 +243,12 @@ orderline AS (
 )
 SELECT
     *,
-    thanh_tien - COALESCE(chiet_khau, 0)  + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0))-- - COALESCE(giam_gia_san_pham, 0) 
+    thanh_tien - COALESCE(chiet_khau, 0)  + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)) - COALESCE(giam_gia_san_pham, 0) 
     AS tien_khach_hang_thanh_toan,
-    thanh_tien  --- COALESCE(giam_gia_san_pham, 0)
+    thanh_tien  - COALESCE(giam_gia_san_pham, 0)
     AS tien_sp_sau_tro_gia,
     COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0) AS gia_ban_daily_total,
-    (COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0)) - (thanh_tien - COALESCE(chiet_khau, 0) )--- COALESCE(giam_gia_san_pham, 0)) 
+    (COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0)) - (thanh_tien - COALESCE(chiet_khau, 0) - COALESCE(giam_gia_san_pham, 0)) 
     AS tien_chiet_khau_sp,
  
     (thanh_tien - COALESCE(chiet_khau, 0)) -- + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)))
