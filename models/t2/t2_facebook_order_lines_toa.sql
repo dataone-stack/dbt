d389758ddd33176af219e77778c84e6ab138c49c
Huy ta <data@maxeagle.net>
2025-08-22 07:26:03 +0000

    df

--- models/t2/t2_facebook_order_lines_toa.sql
+++ models/t2/t2_facebook_order_lines_toa.sql
@@ -152,6 +152,7 @@ case
   then 'Đăng đơn nợ hàng'
   when b.loai_don_no is null and a.status = 'Đăng đơn'
   then 'Đăng đơn không nợ hàng'
+  else '-'
 end as status_dang_don
 from a
 left join {{ref("t2_pancake_pos_don_no_total")}} as b on a.ma_don_hang = b.ma_don_hang and a.brand = b.brand and a.sku_code = b.sku_code
