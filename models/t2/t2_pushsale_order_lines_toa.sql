b5c92b096afc3000184dad6f8500cfa18f2852ef
Huy ta <data@maxeagle.net>
2025-09-09 06:55:52 +0000

    bỏ làm tròn

--- models/t2/t2_pushsale_order_lines_toa.sql
+++ models/t2/t2_pushsale_order_lines_toa.sql
@@ -247,22 +247,22 @@ orderline AS (
 )
 SELECT
     *,
-    ROUND(thanh_tien - COALESCE(chiet_khau, 0) - COALESCE(giam_gia_san_pham, 0) + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)),-1) AS tien_khach_hang_thanh_toan,
+    thanh_tien - COALESCE(chiet_khau, 0) - COALESCE(giam_gia_san_pham, 0) + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)) AS tien_khach_hang_thanh_toan,
     thanh_tien - COALESCE(giam_gia_san_pham, 0) AS tien_sp_sau_tro_gia,
     COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0) AS gia_ban_daily_total,
     (COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0)) - (thanh_tien - COALESCE(chiet_khau, 0) - COALESCE(giam_gia_san_pham, 0)) AS tien_chiet_khau_sp,
  
-    ROUND((thanh_tien - COALESCE(chiet_khau, 0)),-1) -- + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)))
+    (thanh_tien - COALESCE(chiet_khau, 0)) -- + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)))
      AS doanh_thu_ke_toan,
     CASE 
         WHEN loai_khach_hang = 'Khách hàng mới' 
-        THEN ROUND(thanh_tien - chiet_khau,-1)
+        THEN thanh_tien - chiet_khau
         ELSE 0
     END AS doanh_so_moi,
 
     CASE 
         WHEN loai_khach_hang = 'Khách hàng cũ' 
-        THEN ROUND(thanh_tien - chiet_khau,-1)
+        THEN thanh_tien - chiet_khau
         ELSE 0
     END AS doanh_so_cu
 FROM orderline
