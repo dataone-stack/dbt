dc3b3f0efaca604158ec31ae639f4050e8dbd400
Huy ta <data@maxeagle.net>
2025-09-10 10:23:33 +0000

    update thêm phí ship vào cột doanh thu kế toán

--- models/t2/t2_pushsale_order_lines_toa.sql
+++ models/t2/t2_pushsale_order_lines_toa.sql
@@ -258,8 +258,7 @@ SELECT
     COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0) AS gia_ban_daily_total,
     (COALESCE(gia_ban_daily, 0) * COALESCE(so_luong, 0)) - (thanh_tien - COALESCE(chiet_khau, 0) - COALESCE(giam_gia_san_pham, 0)) AS tien_chiet_khau_sp,
  
-    (thanh_tien - COALESCE(chiet_khau, 0)) -- + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0)))
-     AS doanh_thu_ke_toan,
+    (thanh_tien - COALESCE(chiet_khau, 0) + (COALESCE(gia_dich_vu_vc, 0) - COALESCE(phi_vc_ho_tro_khach, 0))) AS doanh_thu_ke_toan,
     CASE 
         WHEN loai_khach_hang = 'Khách mới' 
         THEN thanh_tien - chiet_khau
