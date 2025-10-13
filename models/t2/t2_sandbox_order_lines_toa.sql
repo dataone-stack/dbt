4a3ccb3ed674cf95abb6674a1aa379f018d366d4
Huy ta <data@maxeagle.net>
2025-10-13 07:32:52 +0000

    update marketing display name

--- models/t2/t2_sandbox_order_lines_toa.sql
+++ models/t2/t2_sandbox_order_lines_toa.sql
@@ -95,10 +95,14 @@ orderline AS (
         -- Nhân sự liên quan
         CASE 
             WHEN (ord.marketing_display_name IS NULL OR ord.marketing_display_name = '') THEN 'Admin Đơn vị'
-            ELSE mar.marketing_name
+            ELSE COALESCE(mar.marketing_name, ord.marketing_display_name)
         END AS marketing_name,
-    
-        mar.ma_nhan_vien,
+
+        CASE 
+            WHEN (ord.marketing_user_name IS NULL OR ord.marketing_user_name = '') THEN 'admin'
+            ELSE COALESCE(ord.marketing_user_name, mar.marketer_name)
+        END AS ma_nhan_vien,
+
         COALESCE(mar.ma_quan_ly, mar2.ma_quan_ly) AS ma_quan_ly,
         COALESCE(mar.manager, mar2.manager) AS manager,
 
