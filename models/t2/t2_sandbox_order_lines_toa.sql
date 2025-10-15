f8a7e801280eab7684cca0bb5be390fd0f3e82a9
Huy ta <data@maxeagle.net>
2025-10-15 03:04:05 +0000

    update ds mới

--- models/t2/t2_sandbox_order_lines_toa.sql
+++ models/t2/t2_sandbox_order_lines_toa.sql
@@ -42,10 +42,16 @@ orderline AS (
         ) AS dia_chi,
         ord.delivery_province_name AS tinh_giao_hang,
 
-        CASE 
-            WHEN ord.customer_type = 0 THEN 'Khách mới'
-            WHEN ord.customer_type = 1 THEN 'Khách cũ'
-            ELSE 'Không xác định'
+        -- CASE 
+        --     WHEN ord.customer_type = 0 THEN 'Khách mới'
+        --     WHEN ord.customer_type = 1 THEN 'Khách cũ'
+        --     ELSE 'Không xác định'
+        -- END AS loai_khach_hang,
+
+        CASE
+            WHEN LOWER(ord.source_name) LIKE '%khách cũ%' THEN 'Khách cũ'
+            WHEN ord.reason_to_create IN ('FOR_TAKE_CARE', 'FROM_OLD', 'OLD_ORDER', 'TAKECARE') THEN 'Khách cũ'
+            ELSE 'Khách mới'
         END AS loai_khach_hang,
 
         CASE
