0cf3dd866f67b47131cd21f147cd51db60db9f82
Huy ta <data@maxeagle.net>
2025-10-30 07:51:59 +0000

    f

---
+++ models/t1/t1_vietful_nhapkho_detail_total.sql
@@ -0,0 +1,5 @@
+SELECT *,'Chaching' as brand FROM `crypto-arcade-453509-i8.chaching_vietful_dwh.vietful_inbound_request_lyb`
+UNION ALL
+SELECT *,'LYB' as brand FROM `crypto-arcade-453509-i8.lyb_vietful_dwh.vietful_inbound_request_lyb`
+UNION ALL
+SELECT *,'UME' as brand FROM `crypto-arcade-453509-i8.ume_vietful_dwh.vietful_inbound_request_lyb`
