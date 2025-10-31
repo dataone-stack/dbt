c987aeb4293887e4d0fce83e89f9b9c5277f1f12
Tran Thi Kieu Trinh <kieutrinhtran2212@gmail.com>
2025-10-31 08:53:31 +0700

    Revert "up"

--- models/demo/t3_pnl_revenue.sql
+++ models/demo/t3_pnl_revenue.sql
@@ -21,9 +21,7 @@ SELECT
     0 as doanh_so_moi,
     'Facebook' AS channel,
     phi_van_chuyen_thuc_te,
-    gia_von_total,
     gia_von,
-    so_luong,
     promotion_type
 FROM `crypto-arcade-453509-i8`.`dtm`.`pnl_t2_facebook`
 where CAST(ngay_da_giao AS TIMESTAMP) is not null
@@ -90,10 +88,7 @@ SELECT
     0 as doanh_so_moi,
     'Tiktok' AS channel,
     0 as phi_van_chuyen_thuc_te,
-    gia_von_total,
     gia_von,
-    so_luong,
-    
     promotion_type
 FROM `crypto-arcade-453509-i8`.`dtm`.`t2_tiktok_order_line_tot`
 -- where status not in ("Đã hủy", "Đang giao")
