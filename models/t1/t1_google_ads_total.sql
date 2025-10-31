5eb968a28f4fa689e0a65fcf7814134a247e4ddb
Huy ta <data@maxeagle.net>
2025-10-31 09:38:03 +0000

    f

--- models/t1/t1_google_ads_total.sql
+++ models/t1/t1_google_ads_total.sql
@@ -1,4 +1,4 @@
-with a as (
+
 select segments.date as segment_date,account_id,metrics.costMicros as spend,metrics.conversionsValue,customer.currencyCode as currency from `team_maxeagle_google_ads_dwh.me_ume_googleads_khoavu1211`
 union all
 select segments.date as segment_date,account_id,metrics.costMicros as spend,metrics.conversionsValue,customer.currencyCode as currency from `team_maxeagle_google_ads_dwh.me_ume_googleads_buiducan541992_dwh`
@@ -52,6 +52,4 @@ union all
 select segments.date as segment_date,account_id,metrics.costMicros as spend,metrics.conversionsValue,customer.currencyCode as currency from `team_maxeagle_google_ads_dwh.me_caphemamxoi_googleads_mariegodfrey97790`
 union all
 select segments.date as segment_date,account_id,metrics.costMicros as spend,metrics.conversionsValue,customer.currencyCode as currency from `team_maxeagle_google_ads_dwh.me_be20_googleads_mailbiendongsoduqa01`
-) 
 
-select * from a where date(segment_date) <= '2025-09-31'
