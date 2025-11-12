6c7fdcffa771a7ea3c78c4c7ff9d1efb3bdfa8ef
Huy ta <data@maxeagle.net>
2025-11-12 04:36:48 +0000

    update field ds mới cũ

--- models/t1/t1_kpi_ads_total.sql
+++ models/t1/t1_kpi_ads_total.sql
@@ -1,9 +1,23 @@
-SELECT *,
+SELECT
+    year,
+    month,
+    brand,
+    channel,
+    manager_code,
+    manager,
+    ma_nhan_vien,
+    staff,
+    revenue_target AS kpi_ds_tong,
+    cir_target AS kpi_cir_tong,
+    spend AS kpi_chi_tieu,
+    0 AS kpi_ds_moi,
+    0 AS kpi_cir_moi,
+    0 AS kpi_ds_cu,
     'One5' AS company
     FROM `google_sheet.one5_kpi_ads`
-    WHERE year IS NOT NULL
+    WHERE year IS NOT NULL 
 UNION ALL
 SELECT * ,
     'Max Eagle' AS company
-    FROM `google_sheet.me_kpi_ads`
+    FROM `google_sheet.me_kpi_ad`
     WHERE year IS NOT NULL and month >8
