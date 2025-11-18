82147d5ad394a554ad7dc66d96a3c6e925be3d63
Huy ta <data@maxeagle.net>
2025-11-18 02:43:33 +0000

    update tongphisan

--- models/t3/t3_ads_total_with_tkqc.sql
+++ models/t3/t3_ads_total_with_tkqc.sql
@@ -276,11 +276,11 @@ FROM ads_ladipageFacebook_total_with_tkqc AS ads
 )
 ,
 shop as (
-  select date(ngay_tao_don) as ngay_tao_don,shop_id, sum(tien_khach_hang_thanh_toan) as doanh_thu_san from {{ref("t3_revenue_all_channel")}} --where shop_id in ('VNLCQUWAWR','1360134442') and date(ngay_tao_don) between'2025-11-09' and '2025-11-10'
+  select date(ngay_tao_don) as ngay_tao_don,shop_id, sum(doanh_so_san) as doanh_so_san, sum(tong_phi_san) as tong_phi_san from {{ref("t3_revenue_all_channel")}} --where shop_id in ('VNLCQUWAWR','1360134442') and date(ngay_tao_don) between'2025-11-09' and '2025-11-10'
   group by date(ngay_tao_don),shop_id
 )
 
-select a.*, c.doanh_thu_san from a
+select a.*, c.doanh_so_san,c.tong_phi_san from a
 left join `google_sheet.one_shop_id` b on a.idtkqc = cast(b.idtkqc as string)
 left join shop c on b.shopid = c.shop_id and date(a.date_start) = date(c.ngay_tao_don) and a.loaiDoanhThu in ('TikTok GMVmax', 'Shopee Ads')
 
