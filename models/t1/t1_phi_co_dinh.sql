11da50b72e963dab4faf45e558e24c2fd040e017
Huy ta <data@maxeagle.net>
2025-12-01 09:53:10 +0000

    update phí cố định

--- models/t1/t1_phi_co_dinh.sql
+++ models/t1/t1_phi_co_dinh.sql
@@ -1,2 +1,11 @@
-select * from crypto-arcade-453509-i8.google_sheet.chi_phi_co_dinh_pl
+select 
+SAFE_CAST(year as INT64) as year, 
+SAFE_CAST(month as INT64) as month, 
+brand, 
+layer1, 
+layer2, 
+SAFE_CAST(total as FLOAT64) as total, 
+type_of_calculation, 
+company
+ from crypto-arcade-453509-i8.google_sheet.chi_phi_co_dinh_pl
 -- WHERE ma_sku IS NOT NULL
